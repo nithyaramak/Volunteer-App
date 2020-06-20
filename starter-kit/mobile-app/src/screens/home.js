6c6c6f42a7ea307cb3599e0f75441e37e0c4a07b
Mayuri Thakur <mayuri.thakur@startv.com>
2020-06-20 13:23:19 +0530

    home.js

--- starter-kit/mobile-app/src/screens/home.js
+++ starter-kit/mobile-app/src/screens/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Image, Text, TouchableOpacity, Button, Linking } from 'react-native';
+import { StyleSheet, View, Image, Text, TouchableOpacity, Button, Linking, ImageBackground } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
     paddingBottom: 25,
-    paddingTop: 75
+    paddingTop: 120
   },
   image: {
     alignSelf: 'flex-start',
@@ -26,7 +26,8 @@ const styles = StyleSheet.create({
     fontFamily: 'IBMPlexSans-Medium',
     fontSize: 36,
     color: '#323232',
-    paddingBottom: 15
+    paddingBottom: 10,
+    fontWeight: "bold"
   },
   subtitle: {
     fontFamily: 'IBMPlexSans-Light',
@@ -42,7 +43,8 @@ const styles = StyleSheet.create({
     color: '#323232',
     marginTop: 10,
     marginBottom: 10,
-    fontSize: 16
+    fontSize: 16,
+    backgroundColor: '#fff'
   },
   buttonGroup: {
     flex: 1,
@@ -58,12 +60,24 @@ const styles = StyleSheet.create({
     padding: 12,
     textAlign:'center',
     marginTop: 15
+  },
+  backgroundImage: {
+    flex: 1,
+    width:null,
+    height: 120,
+    zIndex:-1,
+    resizeMode: 'cover', // or 'stretch'
   }
 });
 
 const Home = () => (
   <View style={styles.center}>
+        <ImageBackground
+        style={styles.backgroundImage}
+        source={require('../images/GettyImages-1188864563Yevhenii-Dubinko.jpg')}
+      >
     <ScrollView style={styles.scroll}>
+
       <Text style={styles.title}>Guardian Angels</Text>
       <Text style={styles.content}>
         There is a great deal of satisfaction that comes from making a difference to people in your local community.
@@ -75,7 +89,9 @@ const Home = () => (
       <Text style={styles.content}>
       Our motive is to build a self-sufficient society, connecting the helping hands to the needy ones.
       </Text>
+    
     </ScrollView>
+    </ImageBackground>
   </View>
 );
 
